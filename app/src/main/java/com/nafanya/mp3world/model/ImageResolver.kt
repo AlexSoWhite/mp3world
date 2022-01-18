6b9f152652ea7f31c28ec7e2f1a1915d99c93fdb
AlexSoWhite <shura_belov_00@bk.ru>
2022-01-18 21:54:53 +0300

    media picture and fullscreen player init

---
+++ app/src/main/java/com/nafanya/mp3world/model/ImageResolver.kt
@@ -0,0 +1,21 @@
+package com.nafanya.mp3world.model
+
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+object ImageResolver {
+
+//    fun songImage(song: Song, view: ImageView) {
+//        val retriever = MediaMetadataRetriever()
+//        retriever.setDataSource(song.path)
+//        val art = retriever.embeddedPicture
+//        if (art != null) {
+//            val bitmap = BitmapFactory.decodeByteArray(art, 0, art.size)
+//            Glide.with(view).load(bitmap).into(view)
+//        } else {
+//            Glide.with(view).load(null).into(view)
+//        }
+//    }
+}
