9eb028e7956dac3e3f79d5757c5d5cab40581ad5
AlexSoWhite <shura_belov_00@bk.ru>
2022-01-18 21:59:47 +0300

    resetting playlist bug fixed

--- app/src/main/java/com/nafanya/mp3world/view/SongListFragment.kt
+++ app/src/main/java/com/nafanya/mp3world/view/SongListFragment.kt
@@ -1,12 +1,18 @@
 package com.nafanya.mp3world.view
 
 import android.util.Log
+import com.nafanya.mp3world.model.Playlist
 import com.nafanya.mp3world.model.SongListManager
+import com.nafanya.mp3world.viewmodel.ForegroundServiceLiveDataProvider
 
 class SongListFragment : RecyclerHolderFragment() {
 
     override fun setAdapter() {
-        binding.recycler.adapter = SongListAdapter(SongListManager.getSongList()) {}
+        binding.recycler.adapter = SongListAdapter(SongListManager.getSongList()) {
+            ForegroundServiceLiveDataProvider.currentPlaylist.value = Playlist(
+                SongListManager.getSongList()
+            )
+        }
     }
 
     override fun getItemCount(): Int {
