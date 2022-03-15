2ef6c830d646b53c9feeabc72272904ab3a8b038
AlexSoWhite <shura_belov_00@bk.ru>
2022-03-15 18:57:42 +0300

    current playlist display in full screen player activity

---
+++ app/src/main/java/com/nafanya/mp3world/view/listActivities/playlists/CurrentPlaylistDialogActivity.kt
@@ -0,0 +1,38 @@
+package com.nafanya.mp3world.view.listActivities.playlists
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nafanya.mp3world.R
+import com.nafanya.mp3world.databinding.PlaylistPopupActivityBinding
+import com.nafanya.mp3world.model.foregroundService.ForegroundServiceLiveDataProvider
+import com.nafanya.mp3world.model.wrappers.Playlist
+import com.nafanya.mp3world.view.listActivities.songs.SongListAdapter
+
+class CurrentPlaylistDialogActivity : AppCompatActivity() {
+
+    private lateinit var binding: PlaylistPopupActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.playlist_popup_activity)
+        val attrs = window.attributes
+        attrs.gravity = Gravity.BOTTOM
+        attrs.width = WindowManager.LayoutParams.MATCH_PARENT
+        window.attributes = attrs
+        binding.playlist.layoutManager = LinearLayoutManager(this)
+        val playlist = ForegroundServiceLiveDataProvider.currentPlaylist.value
+        binding.playlist.adapter = SongListAdapter(
+            playlist!!.songList,
+            this,
+        ) {
+            ForegroundServiceLiveDataProvider.currentPlaylist.value = playlist
+        }
+        binding.playlistTitle.text = playlist.name
+    }
+}
