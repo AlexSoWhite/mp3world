1d657b56c790a79ecb32ce0446f1f58d97bcd7c5
AlexSoWhite <shura_belov_00@bk.ru>
2022-03-28 22:35:32 +0300

    linter

--- app/src/main/java/com/nafanya/mp3world/view/listActivities/playlists/PlaylistListActivity.kt
+++ app/src/main/java/com/nafanya/mp3world/view/listActivities/playlists/PlaylistListActivity.kt
@@ -1,13 +1,9 @@
 package com.nafanya.mp3world.view.listActivities.playlists
 
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.view.View
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.nafanya.mp3world.R
 import com.nafanya.mp3world.model.wrappers.Playlist
 import com.nafanya.mp3world.view.listActivities.RecyclerHolderActivity
 import com.nafanya.mp3world.viewmodel.listViewModels.SourceProvider
@@ -31,23 +27,6 @@ class PlaylistListActivity : RecyclerHolderActivity() {
                         startActivity(intent)
                     }
                     ClickType.LONG -> {
-//                        val builder = AlertDialog.Builder(this, R.style.AlertDialog)
-//                        builder
-//                            .setMessage(getString(R.string.delete_playlist, playlist.name))
-//                            .setPositiveButton(
-//                                "удалить",
-//                                DialogInterface.OnClickListener { dialogInterface, i ->
-//                                    (viewModel as PlaylistListViewModel).deletePlaylist(playlist)
-//                                    dialogInterface.dismiss()
-//                                })
-//                            .setNegativeButton(
-//                                "отмена",
-//                                DialogInterface.OnClickListener { dialogInterface, i ->
-//                                    dialogInterface.dismiss()
-//                                }
-//                            )
-//                        val alertDialog = builder.create()
-//                        alertDialog.show()
                         val intent = Intent(this, DeletePlaylistDialogActivity::class.java)
                         DeletePlaylistDialogActivity.prepare(
                             viewModel as PlaylistListViewModel,
