3da631fe5f2f8c724d7fd156b23bd7cf2facda40
AlexSoWhite <shura_belov_00@bk.ru>
2022-04-09 21:18:43 +0300

    fullscreen player background and text colors

--- app/src/main/java/com/nafanya/mp3world/view/ColorExtractor.kt
+++ app/src/main/java/com/nafanya/mp3world/view/ColorExtractor.kt
@@ -2,26 +2,46 @@ package com.nafanya.mp3world.view
 
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.drawable.Drawable
 import android.os.Build
 import androidx.annotation.RequiresApi
 
 object ColorExtractor {
 
     @RequiresApi(Build.VERSION_CODES.Q)
-    fun getAverageColor(bitmap: Bitmap): Color {
-        var averageRed = 0.0F
-        var averageGreen = 0.0F
-        var averageBlue = 0.0F
+    fun getAverageColorWithNoWhiteComponent(bitmap: Bitmap): Int {
+        var averageRed: Double = 0.0
+        var averageGreen: Double = 0.0
+        var averageBlue: Double = 0.0
         val width = bitmap.width
         val height = bitmap.height
+        var count: Long = 0
         for (i in 0 until width) {
             for (j in 0 until height) {
                 val color = bitmap.getColor(i, j)
-                averageRed += color.red() / (width * height)
-                averageGreen += color.green() / (width * height)
-                averageBlue += color.blue() / (width * height)
+                if (color.red() + color.green() + color.blue() < 2.4f) {
+                    count++
+                    averageRed += color.red()
+                    averageGreen += color.green()
+                    averageBlue += color.blue()
+                }
             }
         }
-        return Color.valueOf(averageRed, averageGreen, averageBlue)
+        averageRed /= count
+        averageGreen /= count
+        averageBlue /= count
+        return Color.valueOf(averageRed.toFloat(), averageGreen.toFloat(), averageBlue.toFloat()).toArgb()
+    }
+
+    private val negative = floatArrayOf(
+        -1.0f,     .0f,     .0f,    .0f,  255.0f,
+        .0f,   -1.0f,     .0f,    .0f,  255.0f,
+        .0f,     .0f,   -1.0f,    .0f,  255.0f,
+        .0f,     .0f,     .0f,   1.0f,     .0f
+    )
+
+    fun Drawable.toNegative() {
+        this.colorFilter = ColorMatrixColorFilter(negative)
     }
 }
--- app/src/main/java/com/nafanya/mp3world/view/playerViews/FullScreenPlayerActivity.kt
+++ app/src/main/java/com/nafanya/mp3world/view/playerViews/FullScreenPlayerActivity.kt
@@ -4,6 +4,8 @@ import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -14,6 +16,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.toColor
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
@@ -21,6 +24,7 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.google.android.exoplayer2.ui.DefaultTimeBar
 import com.google.android.material.imageview.ShapeableImageView
 import com.nafanya.mp3world.R
 import com.nafanya.mp3world.databinding.PlayerViewFullscreenBinding
@@ -39,6 +43,7 @@ class FullScreenPlayerActivity : AppCompatActivity() {
     private lateinit var binding: PlayerViewFullscreenBinding
     private var playerView: FullScreenPlayerView? = null
     private var previousColor: Int = -1
+    private var isColorInitialized = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,76 +86,39 @@ class FullScreenPlayerActivity : AppCompatActivity() {
                 when {
                     song.art != null -> {
                         songIcon.setImageBitmap(song.art)
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                            val averageColor = ColorExtractor.getAverageColor(song.art).toArgb()
-                            val colorFrom =
-                                if (previousColor != -1) {
-                                    previousColor
-                                } else {
-                                    previousColor = averageColor
-                                    getColor(android.R.color.transparent)
-                                }
-                            val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, averageColor)
-                            colorAnimation.duration = backgroundDuration
-                            colorAnimation.addUpdateListener {
-                                binding.fullScreenPlayerView.setBackgroundColor(it.animatedValue as Int)
-                            }
-                            colorAnimation.start()
-                        }
+                        animateChanges(song.art)
                     }
                     song.artUrl != null -> {
-                        val bitmap = Glide.with(songIcon).asBitmap()
-                        bitmap.addListener(object : RequestListener<Bitmap> {
-                            override fun onLoadFailed(
-                                e: GlideException?,
-                                model: Any?,
-                                target: Target<Bitmap>?,
-                                isFirstResource: Boolean
-                            ): Boolean {
-                                Log.d("Image", e.toString())
-                                return false
-                            }
-
-                            override fun onResourceReady(
-                                resource: Bitmap?,
-                                model: Any?,
-                                target: Target<Bitmap>?,
-                                dataSource: DataSource?,
-                                isFirstResource: Boolean
-                            ): Boolean {
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                                    songIcon.setImageBitmap(resource)
-                                    val averageColor = ColorExtractor.getAverageColor(resource!!).toArgb()
-                                    val colorFrom =
-                                    if (previousColor != -1) {
-                                        previousColor
-                                    } else {
-                                        previousColor = averageColor
-                                        getColor(android.R.color.transparent)
-                                    }
-                                    val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, averageColor)
-                                    colorAnimation.duration = backgroundDuration
-                                    colorAnimation.addUpdateListener {
-                                        binding.fullScreenPlayerView.setBackgroundColor(it.animatedValue as Int)
-                                    }
-                                    colorAnimation.start()
-                                    return true
-                                }
-                                return false
-                            }
-                        })
-                        bitmap.load(song.artUrl).into(songIcon)
-//                        val bitmap = Glide.with(songIcon).asBitmap()
+                        val bitmap = Glide.with(songIcon).load(song.artUrl).into(songIcon)
+                        binding.fullScreenPlayerView.setBackgroundColor(Color.parseColor("#373232"))
+//                        bitmap.addListener(object : RequestListener<Bitmap> {
+//                            override fun onLoadFailed(
+//                                e: GlideException?,
+//                                model: Any?,
+//                                target: Target<Bitmap>?,
+//                                isFirstResource: Boolean
+//                            ): Boolean {
+//                                Log.d("Image", e.toString())
+//                                return false
+//                            }
+//
+//                            override fun onResourceReady(
+//                                resource: Bitmap?,
+//                                model: Any?,
+//                                target: Target<Bitmap>?,
+//                                dataSource: DataSource?,
+//                                isFirstResource: Boolean
+//                            ): Boolean {
+//                                songIcon.setImageBitmap(resource!!)
+//                                animateChanges(resource)
+//                                return true
+//                            }
+//                        })
 //                        bitmap.load(song.artUrl).into(songIcon)
-//                        Glide.with(songIcon).load(song.artUrl).into(songIcon)
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                            val averageColor = ColorExtractor.getAverageColor(bitmap)
-//                            binding.fullScreenPlayerView.setBackgroundColor(averageColor.toArgb())
-//                            songIcon.setImageBitmap(bitmap)
-//                        }
                     }
                     else -> {
                         songIcon.setImageResource(R.drawable.default_placeholder)
+                        animateChanges()
                     }
                 }
                 // favourite
@@ -218,9 +186,61 @@ class FullScreenPlayerActivity : AppCompatActivity() {
         return false
     }
 
+    private fun animateChanges(art: Bitmap? = null) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val averageColor =  if (art != null) {
+                ColorExtractor.getAverageColorWithNoWhiteComponent(art)
+            } else {
+                Color.parseColor("#373232")
+            }
+            val colorFrom = if (isColorInitialized) {
+                previousColor
+            } else {
+                getColor(android.R.color.transparent)
+            }
+            previousColor = averageColor
+            isColorInitialized = true
+            val playPauseButton = findViewById<ShapeableImageView>(R.id.exo_play_pause)
+            val prevButton = findViewById<ShapeableImageView>(R.id.exo_prev)
+            val nextButton = findViewById<ShapeableImageView>(R.id.exo_next)
+            val repeatButton = findViewById<ShapeableImageView>(R.id.exo_repeat_toggle)
+            val playlistButton = findViewById<ShapeableImageView>(R.id.current_playlist)
+            val favouriteButton = findViewById<ShapeableImageView>(R.id.favourite_button)
+            val progressBar = findViewById<DefaultTimeBar>(R.id.exo_progress)
+            val title = findViewById<TextView>(R.id.track_title)
+            val artist = findViewById<TextView>(R.id.track_artist)
+            val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, averageColor)
+            colorAnimation.duration = backgroundDuration
+            colorAnimation.addUpdateListener {
+                val color = (it.animatedValue as Int).toColor()
+                var controlsBlue = color.blue()
+                var controlsRed = color.red()
+                var controlsGreen = color.green()
+                controlsRed = (controlsRed + 2.0f) / 3
+                controlsGreen = (controlsGreen + 2.0f) / 3
+                controlsBlue = (controlsBlue + 2.0f) / 3
+                val controlsColor = Color.valueOf(controlsRed, controlsGreen, controlsBlue).toArgb()
+                val backgroundColor = it.animatedValue as Int
+                binding.fullScreenPlayerView.setBackgroundColor(backgroundColor)
+                playPauseButton.setColorFilter(controlsColor)
+                prevButton.setColorFilter(controlsColor)
+                nextButton.setColorFilter(controlsColor)
+                repeatButton.setColorFilter(controlsColor)
+                playlistButton.setColorFilter(controlsColor)
+                favouriteButton.setColorFilter(controlsColor)
+                progressBar.setBufferedColor(controlsColor)
+                title.setTextColor(controlsColor)
+                artist.setTextColor(controlsColor)
+            }
+            colorAnimation.start()
+        } else {
+            setTheme(R.style.exoPlayer)
+        }
+    }
+
     companion object {
         private const val alphaDuration = 500L
-        private const val backgroundDuration = 500L
+        private const val backgroundDuration = 600L
         private const val startOffset = 350L
     }
 }
