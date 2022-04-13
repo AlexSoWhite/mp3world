51c8b7c86e699f4ed3d5390abc17a15f2b68d099
AlexSoWhite <shura_belov_00@bk.ru>
2022-04-13 23:13:13 +0300

    playlists

--- app/src/main/java/com/nafanya/mp3world/view/listActivities/playlists/PlaylistListActivity.kt
+++ app/src/main/java/com/nafanya/mp3world/view/listActivities/playlists/PlaylistListActivity.kt
@@ -1,12 +1,16 @@
 package com.nafanya.mp3world.view.listActivities.playlists
 
 import android.content.Intent
+import android.view.Menu
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.nafanya.mp3world.R
 import com.nafanya.mp3world.model.wrappers.Playlist
 import com.nafanya.mp3world.view.listActivities.RecyclerHolderActivity
 import com.nafanya.mp3world.viewmodel.listViewModels.SourceProvider
+import com.nafanya.mp3world.viewmodel.listViewModels.albums.AlbumListViewModel
 import com.nafanya.mp3world.viewmodel.listViewModels.playlists.PlaylistListViewModel
 import com.nafanya.mp3world.viewmodel.listViewModels.playlists.PlaylistViewModel
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -62,5 +66,28 @@ class PlaylistListActivity : RecyclerHolderActivity() {
             AddPlaylistDialogActivity.setViewModel(viewModel as PlaylistListViewModel)
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // setting appBar search view
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu?.findItem(R.id.search)
+        val searchView: SearchView = searchItem?.actionView as SearchView
+        // setting search dispatcher
+        searchView.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    (viewModel as PlaylistListViewModel).search(query)
+                    return false
+                }
+                override fun onQueryTextChange(newText: String): Boolean {
+                    if (newText.isEmpty()) {
+                        (viewModel as PlaylistListViewModel).reset()
+                    }
+                    return false
+                }
+            }
+        )
+        return super.onCreateOptionsMenu(menu)
     }
 }
